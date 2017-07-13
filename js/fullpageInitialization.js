f1f6d40d11a76bfec0c96853a11cae13162cd2e9
Max <maxym.fedas@gmail.com>
2017-07-13 14:34:52 +0300

    optimization

--- js/fullpageInitialization.js
+++ js/fullpageInitialization.js
@@ -1,34 +1,32 @@
 $(function() {
   $(document).ready(function() {
 		$('#fullpage').fullpage({
-			afterLoad: function(anchorLink, index) {
-				var content = $(this);
+			onLeave: function(index, nextIndex, direction) {
+        var nextSectionId = '#section-' + nextIndex;
+        var nextSection = $(nextSectionId);
         var logoName = 'bz';
-        if (anchorLink != 'home') {
-          logoName = anchorLink;
+        if (nextSection.data().anchor != 'home') {
+          logoName = nextSection.data().anchor;
         }
         $($('.logo')[0].children[0]).html(`dops <span>.</span> ${logoName}`);
-				if ($(this).data().loadedcontent) return;
-        if ($(this).data().page === "/portfolio.php") {
-          $('.cases-btn').click();
-          $('#fullpage').data().pageLoad = true;
-          return;
-        }
+				if (nextSection.data().loadedcontent) return;
 					$.ajax({
-						url: $(this).data().page,
+						url: nextSection.data().page,
 						type: 'post',
 						dataType: 'html',
 						success: function(data) {
 							var htmlData = $.parseHTML(data);
-							content.html(htmlData);
+							nextSection.html(htmlData);
 						},
 						error: function(e) {
 							console.log('ERROR', e);
 						},
 						complete: function() {
               $('#fullpage').data().pageLoad = true;
-              content.data().loadedcontent = true;
-							$.getScript("/js/script.js", function() {});
+              nextSection.data().loadedcontent = true;
+							$.getScript("/js/script.js", function() {
+                  $('a[href="#proc-tab-1"]').click();
+              });  
 						}
 					});
 			}
